e82a37b668b5f0801b5e9d4cb335e61f863e81f2
YooSejeong <nived3@naver.com>
2024-09-09 20:05:16 +0900

    maraton.py 수정

--- namno/one/maraton.py
+++ namno/one/maraton.py
@@ -1,17 +1,22 @@
+#프로그래머스
 participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
 completion = ["josipa", "filipa", "marina", "nikola"]
 
 def solution(participant, completion):
     memo = {}
-    for i in completion:
-        memo[i] = True
     
     for i in participant:
+        if i in memo :
+            memo[i] += 1
+        else : 
+            memo[i] =1
+    for i in completion:
         if i in memo:
-             del memo[i]
-             print(memo)
-        elif i not in memo:
-            return i
+             memo[i] -= 1
+    for key, value in memo.items():
+        if value == 1:
+            return key
+        
             
 result = solution(participant, completion)
 print(result)
