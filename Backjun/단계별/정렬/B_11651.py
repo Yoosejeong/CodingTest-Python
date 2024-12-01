ce3d60456837c88149fbf04a95bddac8483676ab
YooSejeong <nived3@naver.com>
2024-12-02 00:13:41 +0900

    B_11651 코드 추가

---
+++ Backjun/단계별/정렬/B_11651.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+arr = []
+for _ in range(n):
+    x, y = map(int, sys.stdin.readline().split())
+    arr.append((x,y))
+
+arr.sort(key = lambda x: (x[1], x[0]))
+
+for x,y in arr:
+    print(x,y)
