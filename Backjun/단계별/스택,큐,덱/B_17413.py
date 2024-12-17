7d6d54bd033d540570747c968e11475ef46038f0
YooSejeong <nived3@naver.com>
2024-12-18 00:35:59 +0900

    B_17413.py 코드 추가

--- Backjun/단계별/스택,큐,덱/B_17413.py
+++ Backjun/단계별/스택,큐,덱/B_17413.py
@@ -1,7 +1,19 @@
 import sys
+import re
 s = sys.stdin.readline().strip()
-arr = []
+arr=[]
 result = []
-for x in s.split():
-    arr.append(x)
-
+arr = re.findall(r"<[^>]+>|[^<]+", s)
+for i in arr:
+    if i[0] == "<":
+        result.append(i)
+    else:
+        li=[]
+        li = i.split()
+        count=0
+        for k in li:
+            count+=1
+            result.append(k[::-1])
+            if len(li) >= 2 and count!=len(li):
+                result.append(" ")
+print(''.join(result))
